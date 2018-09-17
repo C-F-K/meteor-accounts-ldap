4ec6f78094cd2d47255799967f78859bf5b837a4
svc-ldap-vpnctrl <svc-ldap-vpnctrl@acc01.vumc.nl>
2018-09-17 12:16:17 +0200

    smol amend

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'cfk:accounts-ldap',
   version: '1.1.1',
-  summary: 'Fork of typ\'s Accounts login for LDAP using ldapjs. Supports anonymous DN search & LDAPS. Added since forking: explicitly declared underscore, pubkey encrypt ldap creds',
+  summary: 'Fork of typ\'s accounts-ldap. Added since forking: reinclude underscore, pubkey encrypt ldap creds',
   git: 'https://github.com/C-F-K/meteor-accounts-ldap',
   documentation: 'README.md'
 });
 
 Npm.depends({
-  jsencrypt: '3.0.0-rc1'
+  jsencrypt: '3.0.0-rc.1'
 });
 
 Package.onUse(function(api) {
